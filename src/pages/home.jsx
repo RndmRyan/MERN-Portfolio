43733e40e6babfcae691a8d553beb957fe5da5e3
Rayyan M. Zia <rayyanzia2944@gmail.com>
2024-07-02 01:45:11 +0500

    The Journey Only Just Began
    Added past single page projects

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,10 +12,37 @@ function home()
     const setpathWordle = () => {
       navigate("/Wordle");
     }
+    const setpathAdvice = () => {
+      navigate("/AdviceGenerator");
+    }
+    const setpathPassword = () => {
+      navigate("/PasswordGenerator");
+    }
+    const setpathJobListing = () => {
+      navigate("/JobListing");
+    }
+    const setpathCarFlyVal = () => {
+      navigate("/CarFlysValidation");
+    }
+    const setpathContactForm = () => {
+      navigate("/ContactForm");
+    }
 
   return (
     <>
       <div className="container">
+        <h2>Individual Projects</h2>
+        <div className="options">
+          <button onClick={setpathAdvice}>Advice Generator</button>
+          <button onClick={setpathPassword}>Password Generator</button>
+        </div>
+        <div className="options">
+          <button onClick={setpathJobListing}>Job Listing</button>
+          <button onClick={setpathCarFlyVal}>CarFlys Validation</button>
+        </div>
+        <div className="options">
+          <button onClick={setpathContactForm}>ContactForm (Not integrated)</button>
+        </div>
         <h2>Play a Game</h2>
         <div className="options">
           <button onClick={setpathTicTacToe}>TicTacToe</button>
